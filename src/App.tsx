a40b6608d97a6469740b34747536f3b1f29af269
Felix Piskunov <felix.piskunov@gmail.com>
2023-04-28 15:09:23 +0300

    Some clean ups

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,8 @@
-import axios from 'axios'
 import 'antd/dist/reset.css'
 import './antd-overrides.css'
 import Header from './components/Header/Header'
 import { BrowserRouter, Route, Routes } from 'react-router-dom'
 import { Home, Questions, Steps, Preview } from './pages'
-
-axios.defaults.baseURL = 'http://localhost:3001'
 
 function App() {
   return (
